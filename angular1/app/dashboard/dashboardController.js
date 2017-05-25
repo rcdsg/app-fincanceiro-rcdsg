1cfb51b8e582b2c31b82bd4d4052106d20f90889
Leonardo Leitão <leonardomleitao@gmail.com>
2017-05-24 23:01:34 -0300

    Ajustando DashboardController para padrao controllerAs

--- angular1/app/dashboard/dashboardController.js
+++ angular1/app/dashboard/dashboardController.js
@@ -1,20 +1,20 @@
 angular.module('primeiraApp').controller('DashboardCtrl', [
-  '$scope',
   '$http',
   'consts',
   DashboardController
 ])
 
-function DashboardController($scope, $http, consts) {
-
-  $scope.getSummary = function() {
+function DashboardController($http, consts) {
+  const vm = this
+  vm.getSummary = function() {
     const url = `${consts.apiUrl}/billingSummary`;
-    $http.get(url).then(function({credit = 0, debt = 0}) {
-      $scope.credit = credit
-      $scope.debt = debt
-      $scope.total = credit - debt
+    $http.get(url).then(function(response) {
+      const {credit = 0, debt = 0} = response.data
+      vm.credit = credit
+      vm.debt = debt
+      vm.total = credit - debt
     })
   }
 
-  $scope.getSummary()
+  vm.getSummary()
 }
