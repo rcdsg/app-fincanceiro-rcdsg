636843f0bea702e7888b55051354acfece2799fb
rodrigo <rodrigo.costa@cantupneus.com.br>
2020-05-09 13:25:09 -0300

    [AJUSTES]

--- angular1/app/dashboard/dashboardController.js
+++ angular1/app/dashboard/dashboardController.js
@@ -1,20 +1,24 @@
-angular.module('primeiraApp').controller('DashboardCtrl', [
-  '$http',
-  'consts',
-  DashboardController
-])
+(function (){
 
-function DashboardController($http, consts) {
-  const vm = this
-  vm.getSummary = function() {
-    const url = `${consts.apiUrl}/billingSummary`;
-    $http.get(url).then(function(response) {
-      const {credit = 0, debt = 0} = response.data
-      vm.credit = credit
-      vm.debt = debt
-      vm.total = credit - debt
-    })
+  angular.module('primeiraApp').controller('DashboardCtrl', [
+    '$http',
+    'consts',
+    DashboardController
+  ])
+
+  function DashboardController($http, consts) {
+    const vm = this  //vm = View Model
+    vm.getSummary = function() {
+      const url = `${consts.apiUrl}/billingSummary`;
+      $http.get(url).then(function(response) {
+        const {credit = 0, debt = 0} = response.data
+        vm.credit = credit
+        vm.debt = debt
+        vm.total = credit - debt
+      })
+    }
+
+    vm.getSummary()
   }
 
-  vm.getSummary()
-}
+})
