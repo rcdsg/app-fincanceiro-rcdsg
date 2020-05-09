75737d99eb8fc4b44acda586c8f0cbd41785ca35
rodrigo <rodrigo.costa@cantupneus.com.br>
2020-05-09 00:39:14 -0300

    [ATUALIZANDO] O curso

--- angular1/app/config/consts.js
+++ angular1/app/config/consts.js
@@ -1,9 +1,9 @@
 angular.module('primeiraApp').constant('consts', {
   appName: 'MEAN - Primeira Aplicação',
   version: '1.0',
-  owner: 'Cod3r',
-  year: '2017',
-  site: 'http://cod3r.com.br',
+  owner: 'Rodrigo Costa',
+  year: '2020',
+  site: 'http://rcdsg.com',
   apiUrl: 'http://localhost:3003/api',
 }).run(['$rootScope', 'consts', function($rootScope, consts) {
   $rootScope.consts = consts
